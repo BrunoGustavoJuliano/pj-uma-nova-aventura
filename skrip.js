88f6072a618f13db62c62424fe7cf620122aabef
Bruno Juliano <bruno.juliano.carvalho@escola.pr.gov.br>
2024-11-26 19:20:10 -0300

    Add files via upload

--- skrip.js
+++ skrip.js
@@ -1,7 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-const avanca = document.querySelectorAll('.btn-proximo');
-
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
